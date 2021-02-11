eadc8dc252859e8169a5202e57403d6a3baf4241
BlueV12 <yosef.ashley.2018@gmail.com>
2021-02-11 15:17:35 -0600

    The value is actually correct, thank you very much.

--- src/main/java/xyz/rainbowpunk/multigames/specialeffects/SpecialEffects.java
+++ src/main/java/xyz/rainbowpunk/multigames/specialeffects/SpecialEffects.java
@@ -3,6 +3,9 @@ package xyz.rainbowpunk.multigames.specialeffects;
 import org.bukkit.entity.Player;
 
 public class SpecialEffects {
+    /**
+     * Deletes the player's inventory in a very exaggerated manner.
+     */
     public static void dissolveInventory(Player player) {
         DissolveInventory.perform(player);
     }
