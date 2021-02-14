b8441f3ced56dda200c9fccf1694957fc0031194
BlueV12 <yosef.ashley.2018@gmail.com>
2021-02-14 14:47:00 -0600

    You now need to be vertically still to step onto a platform.

--- src/main/java/xyz/rainbowpunk/multigames/stages/playerselect/PlayerSelectListener.java
+++ src/main/java/xyz/rainbowpunk/multigames/stages/playerselect/PlayerSelectListener.java
@@ -18,8 +18,11 @@ import java.util.UUID;
  * Picks up events that happen, and calls methods from PlayerSelectStage.
  */
 public class PlayerSelectListener implements Listener {
+    private final static int PLAYER_SHARD_HEIGHT = ColorPlatform.SHARD_HEIGHT + 1;
+    private final static double DETECTION_FUZZINESS = 0.5;
+
     private final PlayerSelectStage stage;
-    private Map<UUID, MultiColor> previouslyStoodMap;
+    private final Map<UUID, MultiColor> previouslyStoodMap;
 
     public PlayerSelectListener(PlayerSelectStage stage) {
         this.stage = stage;
@@ -28,9 +31,7 @@ public class PlayerSelectListener implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
-        UUID uuid = player.getUniqueId();
-        boolean isOnGround = player.getLocation().getY() < 77.5;
+        Player player = event.getPlayer(); UUID uuid = player.getUniqueId();
 
         Block standingOn = getPlayerIndicatorBlock(player);
         MultiColor now = MultiColor.fromBlock(standingOn);
@@ -38,6 +39,11 @@ public class PlayerSelectListener implements Listener {
 
         if (now == last) return; // continue only if the player moved on / off a platform
         if (last != null) stepOff(last, uuid);
+        double yPos = player.getLocation().getY();
+        double yVelocity = Math.abs(player.getVelocity().getY());
+        boolean isOnGround = yPos < PLAYER_SHARD_HEIGHT + DETECTION_FUZZINESS
+                && yVelocity < 0.1;
+
         if (now != null && isOnGround) stepOnto(now, uuid);
 
     }
@@ -57,9 +63,7 @@ public class PlayerSelectListener implements Listener {
         center.setY(ColorPlatform.INDICATOR_HEIGHT);
         if (!isAir(center.getBlock())) return center.getBlock();
 
-        // i apologize for what you're about to witness
-        // ok like the thing is, a player can be standing on a block /without/ their center being directly on top of it
-        // and it's. i don't know what else to do here.
+        // todo: this can be improved so hard
         Block xy = center.clone().add(0.3, 0.1, 0.3).getBlock(); if (!isAir(xy)) return xy;
         Block x_y = center.clone().add(-0.3, 0.0, 0.3).getBlock(); if (!isAir(x_y)) return x_y;
         Block x_y_ = center.clone().add(-0.3, 0.0, -0.3).getBlock(); if (!isAir(x_y_)) return x_y_;
